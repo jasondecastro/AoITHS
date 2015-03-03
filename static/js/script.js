69a44b52d39e99b542354ed2f2999f0281ffa40b
Jovan <jovanjkelly@yahoo.com>
2015-03-03 16:35:59 -0500

    icon rollover and message sent message appear

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $('document').ready(function(){
   $('.message').hide();
   var $btn = $('input[type=submit]');
   $btn.click(function(){
-    $('#five container').fadeOut();
+    $('#five .container').fadeOut();
     $('.message').delay().fadeIn();
   })
 });
