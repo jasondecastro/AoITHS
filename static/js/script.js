c690d0590b33a291dd73de67200639e6fcf2eab3
kellyjovan <jovanjkelly@yahoo.com>
2015-03-03 12:42:11 -0500

    edit index

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+  $('.message').hide();
+  var $btn = $('input[type=submit]');
+  $btn.click(function(){
+    $('#five container').fadeOut();
+    $('.message').delay().fadeIn();
+  })
+});
